bd84651a5c6c677fd9f47bcc29707f052617e07f
Jalil Nouri <nouri.6412@gmail.com>
2022-12-28 16:06:45 +0330

    ddd

--- woo/src/main.ts
+++ woo/src/main.ts
@@ -1,14 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/vendors/general/perfect-scrollbar/css/perfect-scrollbar.css'
-import './assets/vendors/general/socicon/css/socicon.css'
-import './assets/vendors/custom/vendors/line-awesome/css/line-awesome.css'
-import './assets/vendors/custom/vendors/flaticon/flaticon.css'
-import './assets/vendors/custom/vendors/flaticon2/flaticon.css'
-import './assets/vendors/general/@fortawesome/fontawesome-free/css/all.min.css'
-
-import './assets/css/demo1/style.bundle.rtl.min.css'
-import './assets/css/demo1/style.bundle.rtl.min.css'
-
 createApp(App).mount('#app')
